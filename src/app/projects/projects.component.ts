b2f8b4356011b1b1b78441d2c4770d779a7e2993
Yarod Bonilla <yarold_br@outlook.es>
2024-10-31 09:13:15 -0500

    video

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
+  videoUrl: string = 'https://www.youtube.com/embed/mFQFKBN3lDg';
+  sanitizedVideoUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.sanitizedVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.videoUrl
+    );
+  }
 }
